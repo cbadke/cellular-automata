412904baff9554b6f7e3d0b440a5a999ddf28821
Curtis Badke <curtis@badkid.ca>
2013-05-25 13:21:04 -0600

    pass in rule from command line

--- elementary/haskell/cellular.hs
+++ elementary/haskell/cellular.hs
@@ -2,21 +2,24 @@ import Data.Bits
 import Data.Map
 import System.Environment
 
-rule = 38
-
 bitty :: Int -> Int -> [Int]
 bitty width n = reverse [ min 1 (n .&. x) | x <- take width (Prelude.map (2^) [0..])]
 
-
-generation :: [Int] -> [Int]
-generation row = generationR ([0,0] ++ row ++ [0,0])
+automata :: Int -> Int -> [Int]
+automata rule numLines = iter numLines [1]
     where
     ruleMap = fromList (zip (Prelude.map (bitty 3) [0..7]) (bitty 8 rule))
-    generationR row
-        | (length row) < 3 = []
-        | otherwise = (ruleMap ! (take 3 row)) : (generationR (drop 1 row))
+    generation row = generationR ([0,0] ++ row ++ [0,0])
+        where
+        generationR row
+            | (length row) < 3 = []
+            | otherwise = (ruleMap ! (take 3 row)) : (generationR (drop 1 row))
+    iter remaining row
+        | remaining == 0 = row
+        | otherwise = iter (remaining - 1) (generation row)
 
 main = do
     args <- getArgs
     let numLines = read (head args) :: Int
-    putStrLn (show numLines)
+    let rule = read (head (drop 1 args)) :: Int
+    putStrLn (show (automata rule numLines))
