8e90858e637d7250daa07e87ad8e855c7d48ba1a
Curtis Badke <curtis@badkid.ca>
2013-05-25 08:18:55 -0600

    Add function to calculate the next row in elementary automata

--- elementary/haskell/cellular.hs
+++ elementary/haskell/cellular.hs
@@ -3,6 +3,14 @@ import Data.Map
 
 rule = 38
 
+bitty :: Int -> Int -> [Int]
 bitty width n = reverse [ min 1 (n .&. x) | x <- take width (Prelude.map (2^) [0..])]
 
-ruleMap = fromList (zip (Prelude.map (bitty 3) [0..7]) (bitty 8 rule))
+
+generation :: [Int] -> [Int]
+generation row = generationR ([0,0] ++ row ++ [0,0])
+    where
+    ruleMap = fromList (zip (Prelude.map (bitty 3) [0..7]) (bitty 8 rule))
+    generationR row
+        | (length row) < 3 = []
+        | otherwise = (ruleMap ! (take 3 row)) : (generationR (drop 1 row))
